c89cd99dd42c217fb3d4cd8fdf06849aa4090ec0
abaxmahi <mukhtarabbas.gjimt@gmail.com>
2026-01-27 14:00:21 +0530

    completed Task

---
+++ Task_1.py
@@ -0,0 +1,18 @@
+#Perform Basic mathematical Operatrions
+# Write a python program that does the following:
+#1.Takes two numbers as input from the user.
+
+number1 =  int(input("Enter the first number: "))
+number2 = int(input("Enter the 2nd number: "))
+
+#2. perform basic op on these numbers
+
+Addition = number1 + number2
+Subtraction = number1 - number2
+Multiplication = number1 * number2
+Division = number1 / number2
+#3. display the result
+print("Addition: sum is : ",Addition)
+print("Subtraction: difference is: ",Subtraction)
+print("Multiplication: Product is: ",Multiplication)
+print("division: Quotient is: ",Division)
---
+++ Task_2.py
@@ -0,0 +1,12 @@
+#Write a python program that:
+#1. takes a user's first and last name as input
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+#2.concatenates into full name
+full_name = first_name +" " + last_name
+
+#3. print personalized greeting message
+
+print("Hello,",full_name + " ! welcome to the Adventure island.")
