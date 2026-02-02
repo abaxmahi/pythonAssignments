e4d2dd2a03fffffd9732083e1b76771e37de8e17
abaxmahi <mukhtarabbas.gjimt@gmail.com>
2026-02-02 17:11:36 +0530

    Add new task and update previous one

---
+++ Task_1_assignment_2.py
@@ -0,0 +1,7 @@
+#check for Even Odd
+num = int(input('Enter a number: '))
+if num % 2 == 0:
+    print(f"{num} is an even number!.")
+
+else:
+    print(f"{num} is an odd number!.")
---
+++ Task_2_assignment_2.py
@@ -0,0 +1,5 @@
+#sum of integer from 1 to 50 using a loop
+sum = 0
+for i  in range (1,51):
+    sum = sum + i
+print(f"The sum of numbers from 1 to 50 is: {sum}")
